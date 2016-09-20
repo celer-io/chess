24af17ed21a19f80f33a1ea9908ae27b174ab9fc
Clement Lepissier <clem.lepissier@gmail.com>
2016-09-20 07:10:57 +0200

    truc

--- chess.js
+++ chess.js
@@ -66,6 +66,8 @@ function main () {
       board.drawPiece(piece.position, board.createPiece(piece.color, piece.type, piece.id))
     })
   })
+
+
   whiteArmy.map((piece) => {
     return piece.position
   }).map((position) => {
@@ -91,51 +93,36 @@ function main () {
 
 class M {
   getPieceAtPosition (matrix, position, transformation = { x: 0, y: 0 }) {
-    matrix = [
-      ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'],
-      ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8'],
-      ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8'],
-      ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8'],
-      ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'],
-      ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'],
-      ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
-      ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8']
-    ]
-    const letterToX = {
-      h: 0,
-      g: 1,
-      f: 2,
-      e: 3,
-      d: 4,
-      c: 5,
-      b: 6,
-      a: 7
-    }
+    const letterToX = { h: 0, g: 1, f: 2, e: 3, d: 4, c: 5, b: 6, a: 7 }
     position = {
       x: letterToX[position.split('')[0]],
       y: position.split('')[1] - 1
     }
-    console.log(matrix)
-    console.log(position.x)
-    console.log(transformation.x)
-    console.log(position.x + transformation.x)
-    console.log(matrix[position.x + transformation.x])
     return matrix[position.x + transformation.x][position.y + transformation.y]
+  }
+  setPieceAt (matrix, position, piece) {
+    const letterToX = { h: 0, g: 1, f: 2, e: 3, d: 4, c: 5, b: 6, a: 7 }
+    position = {
+      x: letterToX[position.split('')[0]],
+      y: position.split('')[1] - 1
+    }
+    matrix[position.x][position.y] = piece
   }
 }
 
 function initGame () {
-  let gameMatrix = [
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}],
-    [{}, {}, {}, {}, {}, {}, {}, {}]
+  let matrix = [
+    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'],
+    ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8'],
+    ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8'],
+    ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8'],
+    ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'],
+    ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'],
+    ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
+    ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8']
   ]
-  return gameMatrix
+  //TODO : build matrix from armies
+  return matrix
 }
 
 function getWhiteArmy () {
