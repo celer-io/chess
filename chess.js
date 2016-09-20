1a2ccf47c45e09d3ca74d35d9a36037bc00a0da6
Clement Lepissier <clem.lepissier@gmail.com>
2016-09-20 07:07:01 +0200

    refactor event handling, and lots of stuff

--- chess.js
+++ chess.js
@@ -1,174 +1,238 @@
-function main(){
-  let whiteArmy = getWhiteArmy();
-  let blackArmy = getBlackArmy();
+// const R = require('ramda')
+function main () {
+  const rules = new Rules()
+  const board = new Board()
+  const handlers = {
+    squareDragEnter: (ev) => {
+      ev.target.classList.add('move-target')
+    },
+    squareDragOver: (ev) => {
+      ev.preventDefault()
+    },
+    squareDragLeave: (ev) => {
+      ev.target.classList.remove('move-target')
+    },
+    pieceDragEnter: (ev) => {
+      ev.target.parentNode.classList.add('move-target')
+    },
+    pieceDragLeave: (ev) => {
+      ev.target.parentNode.classList.remove('move-target')
+    },
+    squareDrop: (ev) => {
+      ev.preventDefault()
+      const data = JSON.parse(ev.dataTransfer.getData('text/plain'))
+      let squareEl = ev.target
+      let pieceEl
+      if (squareEl.classList.contains('piece')) {
+        squareEl = ev.target.parentNode
+        pieceEl = ev.target
+      }
 
-  const board = new Board(blackArmy, whiteArmy)
+      if (data.possibleMoves.indexOf(squareEl.id) === -1) {
+        console.log('not availableTargets')
+        return
+      }
 
-  window.setTimeout(function(){
-    whiteArmy.pawn1 = board.setPiecePosition(whiteArmy.pawn1, "a4")
+      const newPieceEl = document.getElementById(data.id)
+      if (pieceEl) pieceEl.remove()
+      squareEl.appendChild(newPieceEl)
+    },
+    pieceDragStart: (ev) => {
+      let data = {
+        id: ev.currentTarget.id
+      }
+      console.log(board)
+      board.clearLastMove()
+
+      data.possibleMoves = rules.getMovesFor(gameMatrix, data.id)
+      ev.dataTransfer.setData('text/plain', JSON.stringify(data))
+
+      ev.currentTarget.parentNode.classList.add('move-source')
+
+    // TODO: for nice drag and drop in chrome
+    // event.dataTransfer.setDragImage(image, xOffset, yOffset)
+    }
+
+  }
+
+  let whiteArmy = getWhiteArmy()
+  let blackArmy = getBlackArmy()
+
+  let gameMatrix = initGame()
+
+  //  temporary, we have to model the board as a matrix
+  ;[blackArmy, whiteArmy].forEach((army) => {
+    Object.values(army).forEach((piece) => {
+      board.drawPiece(piece.position, board.createPiece(piece.color, piece.type, piece.id))
+    })
+  })
+  whiteArmy.map((piece) => {
+    return piece.position
+  }).map((position) => {
+    return board.setPieceDraggable(position)
+  }).map((piece) => {
+    piece.addEventListener('dragstart', handlers.pieceDragStart, false)
+  })
+
+  board.getSquares().forEach((square) => {
+    square.addEventListener('dragover', handlers.squareDragOver, false)
+    square.addEventListener('dragenter', handlers.squareDragEnter, false)
+    square.addEventListener('dragleave', handlers.squareDragLeave, false)
+    square.addEventListener('drop', handlers.squareDrop, false)
+  })
+
+  window.setTimeout(function () {
+    board.movePiece('a2', 'a4')
+    var m = new M()
+    var wesd = m.getPieceAtPosition('truc', 'a4')
+    console.log(wesd)
   }, 2000)
 }
 
-
-function getWhiteArmy(){
-  return {
-    king:    {type: "king",  id: "",  color:"white",   position: "e1"},
-    queen:   {type: "queen", id: "",  color:"white",  position: "d1"},
-    rook1:   {type: "rook",  id: "1", color:"white",   position: "a1"},
-    rook2:   {type: "rook",  id: "2", color:"white",   position: "h1"},
-    bishop1: {type: "bishop",id: "1", color:"white", position: "c1"},
-    bishop2: {type: "bishop",id: "2", color:"white", position: "f1"},
-    knight1: {type: "knight",id: "1", color:"white", position: "b1"},
-    knight2: {type: "knight",id: "2", color:"white", position: "g1"},
-    pawn1:   {type: "pawn",  id: "1", color:"white",   position: "a2"},
-    pawn2:   {type: "pawn",  id: "2", color:"white",   position: "b2"},
-    pawn3:   {type: "pawn",  id: "3", color:"white",   position: "c2"},
-    pawn4:   {type: "pawn",  id: "4", color:"white",   position: "d2"},
-    pawn5:   {type: "pawn",  id: "5", color:"white",   position: "e2"},
-    pawn6:   {type: "pawn",  id: "6", color:"white",   position: "f2"},
-    pawn7:   {type: "pawn",  id: "7", color:"white",   position: "g2"},
-    pawn8:   {type: "pawn",  id: "8", color:"white",   position: "h2"}
+class M {
+  getPieceAtPosition (matrix, position, transformation = { x: 0, y: 0 }) {
+    matrix = [
+      ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'],
+      ['g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8'],
+      ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8'],
+      ['e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8'],
+      ['d1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'],
+      ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8'],
+      ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
+      ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8']
+    ]
+    const letterToX = {
+      h: 0,
+      g: 1,
+      f: 2,
+      e: 3,
+      d: 4,
+      c: 5,
+      b: 6,
+      a: 7
+    }
+    position = {
+      x: letterToX[position.split('')[0]],
+      y: position.split('')[1] - 1
+    }
+    console.log(matrix)
+    console.log(position.x)
+    console.log(transformation.x)
+    console.log(position.x + transformation.x)
+    console.log(matrix[position.x + transformation.x])
+    return matrix[position.x + transformation.x][position.y + transformation.y]
   }
 }
 
-function getBlackArmy(){
+function initGame () {
+  let gameMatrix = [
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}],
+    [{}, {}, {}, {}, {}, {}, {}, {}]
+  ]
+  return gameMatrix
+}
+
+function getWhiteArmy () {
+  return [
+    {type: 'king', id: '', color: 'white', position: 'e1'},
+    {type: 'queen', id: '', color: 'white', position: 'd1'},
+    {type: 'rook', id: '1', color: 'white', position: 'a1'},
+    {type: 'rook', id: '2', color: 'white', position: 'h1'},
+    {type: 'bishop', id: '1', color: 'white', position: 'c1'},
+    {type: 'bishop', id: '2', color: 'white', position: 'f1'},
+    {type: 'knight', id: '1', color: 'white', position: 'b1'},
+    {type: 'knight', id: '2', color: 'white', position: 'g1'},
+    {type: 'pawn', id: '1', color: 'white', position: 'a2'},
+    {type: 'pawn', id: '2', color: 'white', position: 'b2'},
+    {type: 'pawn', id: '3', color: 'white', position: 'c2'},
+    {type: 'pawn', id: '4', color: 'white', position: 'd2'},
+    {type: 'pawn', id: '5', color: 'white', position: 'e2'},
+    {type: 'pawn', id: '6', color: 'white', position: 'f2'},
+    {type: 'pawn', id: '7', color: 'white', position: 'g2'},
+    {type: 'pawn', id: '8', color: 'white', position: 'h2'}
+  ]
+}
+
+function getBlackArmy () {
   return {
-    king:    {type: "king",   id: "",  color: "black",   position: "e8"},
-    queen:   {type: "queen",  id: "",  color: "black",  position: "d8"},
-    rook1:   {type: "rook",   id: "1", color: "black",   position: "a8"},
-    rook2:   {type: "rook",   id: "2", color: "black",   position: "h8"},
-    bishop1: {type: "bishop", id: "1", color: "black", position: "c8"},
-    bishop2: {type: "bishop", id: "2", color: "black", position: "f8"},
-    knight1: {type: "knight", id: "1", color: "black", position: "b8"},
-    knight2: {type: "knight", id: "2", color: "black", position: "g8"},
-    pawn1:   {type: "pawn",   id: "1", color: "black",   position: "a7"},
-    pawn2:   {type: "pawn",   id: "2", color: "black",   position: "b7"},
-    pawn3:   {type: "pawn",   id: "3", color: "black",   position: "c7"},
-    pawn4:   {type: "pawn",   id: "4", color: "black",   position: "d7"},
-    pawn5:   {type: "pawn",   id: "5", color: "black",   position: "e7"},
-    pawn6:   {type: "pawn",   id: "6", color: "black",   position: "f7"},
-    pawn7:   {type: "pawn",   id: "7", color: "black",   position: "g7"},
-    pawn8:   {type: "pawn",   id: "8", color: "black",   position: "h7"}
+    king: {type: 'king', id: '', color: 'black', position: 'e8'},
+    queen: {type: 'queen', id: '', color: 'black', position: 'd8'},
+    rook1: {type: 'rook', id: '1', color: 'black', position: 'a8'},
+    rook2: {type: 'rook', id: '2', color: 'black', position: 'h8'},
+    bishop1: {type: 'bishop', id: '1', color: 'black', position: 'c8'},
+    bishop2: {type: 'bishop', id: '2', color: 'black', position: 'f8'},
+    knight1: {type: 'knight', id: '1', color: 'black', position: 'b8'},
+    knight2: {type: 'knight', id: '2', color: 'black', position: 'g8'},
+    pawn1: {type: 'pawn', id: '1', color: 'black', position: 'a7'},
+    pawn2: {type: 'pawn', id: '2', color: 'black', position: 'b7'},
+    pawn3: {type: 'pawn', id: '3', color: 'black', position: 'c7'},
+    pawn4: {type: 'pawn', id: '4', color: 'black', position: 'd7'},
+    pawn5: {type: 'pawn', id: '5', color: 'black', position: 'e7'},
+    pawn6: {type: 'pawn', id: '6', color: 'black', position: 'f7'},
+    pawn7: {type: 'pawn', id: '7', color: 'black', position: 'g7'},
+    pawn8: {type: 'pawn', id: '8', color: 'black', position: 'h7'}
+  }
+}
+
+class Rules {
+  getMovesFor (gameMatrix, position) {
+    console.log('gameMatrix', gameMatrix)
+    console.log('position', position)
+    return [
+      'a3',
+      'b3',
+      'c3',
+      'd3',
+      'e3',
+      'f3',
+      'g3',
+      'h3'
+    ]
   }
 }
 
 class Board {
-  constructor(blackArmy, whiteArmy) {
-    [blackArmy, whiteArmy].forEach((army) => {
-      Object.values(army).forEach((piece) => this.setPiecePosition(piece))
+  movePiece (oldPosition, newPosition) {
+    return this.drawPiece(newPosition, this.getPiece(oldPosition))
+  }
+  setPieceDraggable (position, value = true) {
+    this.getPiece(position).setAttribute('draggable', value)
+    return this.getPiece(position)
+  }
+  removePiece (position) {
+    return this.getPiece(position).remove()
+  }
+  createPiece (color, type, id) {
+    let piece = document.createElement('div')
+    piece.classList.add('piece')
+    piece.classList.add(color + '-' + type)
+    piece.id = color + type + id
+    return piece
+  }
+  drawPiece (position, piece) {
+    return document.getElementById(position).appendChild(piece)
+  }
+  getPiece (position) {
+    return document.getElementById(position).firstChild
+  }
+  getSquares () {
+    return Array.from(document.getElementsByClassName('square'))
+  }
+  clearLastMove () {
+    const moveClasses = ['move-target', 'move-source']
+
+    moveClasses.forEach((moveClass) => {
+      Array.from(document.getElementsByClassName(moveClass))
+        .forEach((squareEl) => {
+          console.log('heyyy')
+          squareEl.classList.remove(moveClass)
+        })
     })
-    this.setupDragHandlers()
-    this.setArmyDraggable(whiteArmy)
-  }
-  setPiecePosition(oldPiece, newPosition) {
-    let newPiece = Object.assign({}, oldPiece)
-    if (newPosition) {
-      this.getPieceEl(oldPiece.position).remove()
-      newPiece.position = newPosition
-    }
-    this.addPieceEl(newPiece)
-
-    return newPiece
-  }
-  setArmyDraggable(army, dragValue = true) {
-    Object.values(army).forEach((piece) => this.setPieceDraggable(piece, dragValue))
-  }
-  setPieceDraggable(piece, dragValue = true) {
-    let pieceEl = this.getPieceEl(piece.position)
-    pieceEl.setAttribute('draggable', dragValue)
-    pieceEl.addEventListener("dragstart", this.pieceDragStart, false)
-  }
-  addPieceEl(piece){
-    let pieceEl = document.createElement("div")
-    pieceEl.classList.add("piece")
-    pieceEl.classList.add(piece.color + "-" + piece.type)
-    pieceEl.id = piece.color + piece.type + piece.id
-
-    pieceEl.addEventListener("dragenter", this.pieceDragEnter, false)
-    pieceEl.addEventListener("dragleave", this.pieceDragLeave, false)
-
-    document.getElementById(piece.position)
-      .appendChild(pieceEl)
-  }
-  getPieceEl(position){
-    return document
-    .getElementById(position)
-    .getElementsByClassName("piece")[0]
-  }
-  setupDragHandlers(){
-    [].forEach.call(document.getElementsByClassName("square"), (squareEl) => {
-      squareEl.addEventListener("dragover", this.squareDragOver, false)
-      squareEl.addEventListener("dragenter", this.squareDragEnter, false)
-      squareEl.addEventListener("dragleave", this.squareDragLeave, false)
-      squareEl.addEventListener("drop", this.squareDrop, false)
-    })
-  }
-  squareDragEnter(ev){
-    ev.target.classList.add("move-target")
-  }
-  squareDragOver(ev){
-    ev.preventDefault()
-  }
-  squareDragLeave(ev){
-    ev.target.classList.remove("move-target")
-  }
-  pieceDragEnter(ev){
-    ev.target.parentNode.classList.add("move-target")
-  }
-  pieceDragLeave(ev){
-    ev.target.parentNode.classList.remove("move-target")
-  }
-  squareDrop(ev){
-    ev.preventDefault()
-    const data = JSON.parse(ev.dataTransfer.getData("text/plain"))
-    let squareEl = ev.target
-    let pieceEl
-    if (squareEl.classList.contains('piece')){
-      squareEl = ev.target.parentNode
-      // ev.target.remove()
-      pieceEl = ev.target
-    }
-    console.log('data.availableTargets',data.availableTargets);
-    console.log('squareEl.id',squareEl.id);
-
-    if(data.availableTargets.indexOf(squareEl.id) === -1) {
-      console.log('not availableTargets');
-      return
-    }
-
-    const newPieceEl = document.getElementById(data.id)
-    if (pieceEl) pieceEl.remove()
-    squareEl.appendChild(newPieceEl)
-  }
-  pieceDragStart(ev){
-    let data = {
-      id: ev.currentTarget.id
-    }
-    data.availableTargets = [
-      "a6",
-      "b6",
-      "c6",
-      "d6",
-      "e6",
-      "f6",
-      "g6",
-      "h6"
-    ]
-    ev.dataTransfer.setData("text/plain", JSON.stringify(data))
-
-    ;[].forEach.call(document.getElementsByClassName("move-target"), (squareEl) => {
-      console.log('heyyy');
-      squareEl.classList.remove("move-target")
-    })
-
-    ;[].forEach.call(document.getElementsByClassName("move-source"), (squareEl) => {
-      squareEl.classList.remove("move-source")
-    })
-
-    ev.currentTarget.parentNode.classList.add("move-source")
-
-    // TODO: for nice drag and drop in chrome
-    // event.dataTransfer.setDragImage(image, xOffset, yOffset);
-
   }
 }
