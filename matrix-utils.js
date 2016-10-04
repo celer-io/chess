d045b711a617b9e03ae15e6f0fe8586891704e0c
Clement Lepissier <clem.lepissier@gmail.com>
2016-10-04 19:33:41 +0200

    made matrix-utils more functional

--- matrix-utils.js
+++ matrix-utils.js
@@ -1,26 +1,30 @@
-
-function getPiece (matrix, position, transformation = { x: 0, y: 0 }) {
-  const coords = positionToCoords(position)
-
-  return matrix[coords.y + transformation.y][coords.x + transformation.x]
+const get = (matrix, coords) => {
+  return matrix[coords.y][coords.x]
 }
 
-function setPiece (matrix, position, piece) {
-  position = positionToCoords(position)
-  matrix[position.y][position.x] = piece
+const set = (matrix, coords, piece) => {
+  matrix[coords.y][coords.x] = piece
   return matrix
 }
 
-function removePiece (matrix, position) {
-  position = positionToCoords(position)
-  delete matrix[position.y][position.x]
+const remove = (matrix, coords) => {
+  delete matrix[coords.y][coords.x]
   return matrix
 }
 
-function updatePiece (matrix, origin, destination) {
+const update = (matrix, origin, destination) => {
   var piece = getPiece(matrix, origin)
   matrix = removePiece(matrix, origin)
   return setPiece(matrix, destination, piece)
+}
+
+const transform = (origin, destination) => {
+  const transformed = {
+    x: origin.x + destination.x,
+    y: origin.y + destination.y
+  }
+  if (transformed.x < 0 || transformed.x > 7 || transformed.y < 0 && transformed.y > 7) return null
+  return transformed
 }
 
 // [['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8'],
@@ -32,7 +36,7 @@ function updatePiece (matrix, origin, destination) {
 //  ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'],
 //  ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8']]
 
-function positionToCoords (position) {
+const coords = (position) => {
   const letterToX = { h: 0, g: 1, f: 2, e: 3, d: 4, c: 5, b: 6, a: 7 }
   return {
     x: letterToX[position.split('')[0]],
@@ -40,28 +44,9 @@ function positionToCoords (position) {
   }
 }
 
-function coordsToPosition (coords) {
+const position = (coords) => {
   const XtoLetter = ['h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
   return XtoLetter[coords.x] + (coords.y + 1)
 }
 
-function getTransformed(position, t){
-  const coords = positionToCoords(position)
-  const transformed = {
-    x: coords.x + t.x,
-    y: coords.y + t.y
-  }
-  if (transformed.x < 0 || transformed.x > 7 || transformed.y < 0 && transformed.y > 7) return null
-
-  return coordsToPosition(transformed)
-}
-
-module.exports = {
-  setPiece,
-  getTransformed,
-  getPiece,
-  removePiece,
-  updatePiece,
-  coordsToPosition,
-  positionToCoords
-}
+module.exports = { set, transform, get, remove, update, position, coords }
